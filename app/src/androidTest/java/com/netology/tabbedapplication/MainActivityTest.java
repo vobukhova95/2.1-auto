1eff1d84fe6ffa2bb19c90a371cbabb5824e6af9
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-12-01 21:58:36 +0700

    Create test

---
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.netology.tabbedapplication;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    String tab = "Tab 2";
+    String pageText = "Page: 2";
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction tabView = onView(withContentDescription(tab));
+        tabView.check(matches(isDisplayed()));
+        tabView.perform(click());
+
+        ViewInteraction textView = onView(withText(pageText));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText(pageText)));
+    }
+}
